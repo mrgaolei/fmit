33d64528e044ba32dfc7b481c4ca67b85ded4828
mrgaolei <gaolei@liheran.com>
2015-08-29 10:34:00 +0800

    分页

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-	url(r'^p/(?P<page>\d*)/$', 'news.views.news_list'),
+	url(r'^p/(?P<page>\d*)/$', 'news.views.news_list', name='news_list_page'),
     url(r'^(?P<id>\d*)\.html$', 'news.views.news_detail', name='news_detail'),
 ]
