efab07d5d453d0d7a0f8adeed47944670dc64848
高磊 <mrgaolei@qq.com>
2015-10-10 10:57:44 +0800

    update STATIC_ROOT

--- fmit/settings.py
+++ fmit/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR + '/static/'
 STATICFILES_DIRS = (
     BASE_DIR + '/static/',
 )
