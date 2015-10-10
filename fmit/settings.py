61eb7e8a0986b329ea8577bc7b77f3b620a3d7f9
高磊 <mrgaolei@qq.com>
2015-10-10 10:58:57 +0800

    update STATICFILES_DIRS

--- fmit/settings.py
+++ fmit/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR + '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR + '/static/',
-)
+STATICFILES_DIRS = ()
 
 TINYMCE_DEFAULT_CONFIG = {
     'theme': 'simple',
