31214d65316cfed850f5e653458b7063dd80c128
高磊 <gaolei@homeba.org>
2016-05-27 15:45:25 +0800

    update

--- audience/urls.py
+++ audience/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
+from audience import views
 
 urlpatterns = [
-	url(r'^$', 'audience.views.index'),
-	url(r'^auth_redirect/(?P<auth_type>\d*)$', 'audience.views.auth_redirect', name='auth_redirect'),
+    url(r'^$', views.index),
+    url(r'^auth_redirect/(?P<auth_type>\d*)$', views.auth_redirect, name='auth_redirect'),
 ]
--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
+from news import views
 
 urlpatterns = [
-    url(r'^p/(?P<page>\d*)/$', 'news.views.news_list', name='news_list_page'),
-    url(r'^(?P<id>\d*)\.html$', 'news.views.news_detail', name='news_detail'),
+    url(r'^p/(?P<page>\d*)/$', views.news_list, name='news_list_page'),
+    url(r'^(?P<id>\d*)\.html$', views.news_detail, name='news_detail'),
 ]
