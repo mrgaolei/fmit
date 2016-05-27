cab8de1e8a3b2af11584cfec82732f3463387f07
高磊 <gaolei@homeba.org>
2016-05-27 16:00:27 +0800

    upgrade

--- audience/urls.py
+++ audience/urls.py
@@ -4,4 +4,5 @@ from audience import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^auth_redirect/(?P<auth_type>\d*)$', views.auth_redirect, name='auth_redirect'),
+    url(r'^auth_callback/(?P<auth_type>\d*)$', views.auth_callback, name='auth_callback'),
 ]
--- audience/views.py
+++ audience/views.py
@@ -1,12 +1,15 @@
 # coding=UTF-8
+from django.core.urlresolvers import reverse
 from weibo import APIClient
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .models import WEIBO, THIRDAUTH_CHOICES
+from .models import THIRDAUTH_CHOICES, WEIBO, WEIXIN
 
 
-def _create_client():
-    return APIClient(3029223458, '9183d1ae117f5cf5a2d03d029fe1f0c1', 'http://fmit.cn/admin/fmcore/userext/wbcb/')
+def _create_client(request):
+    return APIClient(3029223458,
+                     '9183d1ae117f5cf5a2d03d029fe1f0c1',
+                     request.build_absolute_uri(reverse('audience:auth_callback', args=(WEIBO,))))
 
 
 # Create your views here.
@@ -16,8 +19,19 @@ def index(request):
 
 def auth_redirect(request, auth_type):
     if int(auth_type) == WEIBO:
-        client = _create_client()
+        client = _create_client(request)
+    elif int(auth_type) == WEIXIN:
+        raise Exception()
     return redirect(client.get_authorize_url())
+
+
+def auth_callback(request, auth_type):
+    if int(auth_type) == WEIBO:
+        return cb_weibo(request)
+    elif int(auth_type) == WEIXIN:
+        return cb_weixin(request)
+    else:
+        return None
 
 
 def cb_weibo(request):
